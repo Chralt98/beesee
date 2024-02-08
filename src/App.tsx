c0738c0879dc590d038e86abf6c6bfa11d2348d9
Chralt98 <chralt98@gmail.com>
2024-02-08 06:50:16 +0100

    add deploy github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo2 from './web3-approved_1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <p>
+          This website serves as a placeholder for a future blockchain business.
+
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://ipfs.apillon.io/ipns/k2k4r8osp5cj66k364xf308w40wrcj3mg7illmptgdl7mr2w41zaf8dd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          One of a hundred websites hosted for 100 years.
+        </a>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
